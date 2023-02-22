24ed58a6b6f7330c0043449e84955141596a05df
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-02-22 23:29:19 +0530

    trending_movies->updated

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const api_key = 'api_key=0bd3a18560f450a76a32f637f6354d01';
 const base_url = 'https://api.themoviedb.org/3';
-const trend_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
 const img_url = 'https://image.tmdb.org/t/p/w500'
-// console.log(TREND_URL) debugging
 
+const trend_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
 const trending = document.getElementById('trending')
 
 getTrending(trend_url)
@@ -23,3 +22,32 @@ function getTrending(url){
 
 }
 
+function showTrending(datas){
+
+    trending.innerHTML=''
+    datas.forEach(movie =>{
+        const {poster_path,title,vote_average,release_date,original_language    } = movie
+        const tre_movie = document.createElement('div');
+        tre_movie.classList.add("movies-card");
+        // tre_movie.classList.add('movies-cont-wrap');
+
+        tre_movie.innerHTML = `
+
+        <img
+                src="${img_url+poster_path}"
+                alt="${title}"
+                />
+
+             <h3 class="movie-title">${title}</h3>
+
+             <div class="hidden-movie-det">
+                <div><span class='hl'>Movie Name:</span>${title}</div>
+                <div><span class='hl'>Rating:</span> ${vote_average}</div>
+                <div><span class="hl">Release Date:</span>${release_date}</div>
+                <div><span class="hl">Languages:</span> ${original_language}</div>
+                <button>See More</button>
+             </div>   `
+
+             trending.appendChild(tre_movie);
+    })
+}
