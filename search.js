459f464f5bef3c0e090a3d258e20946596e43828
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-02-28 23:43:00 +0530

    search-completed

--- search.js
+++ search.js
@@ -1,13 +1,16 @@
-const api_key = 'api_key=0bd3a18560f450a76a32f637f6354d01';
-const base_url = 'https://api.themoviedb.org/3';
-const img_url = 'https://image.tmdb.org/t/p/w500'
+// const api_key = '0bd3a18560f450a76a32f637f6354d01';
+// const base_url = 'https://api.themoviedb.org/3';
+// const img_url = 'https://image.tmdb.org/t/p/w500'
 
-const trend_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
+// const trend_url = base_url + '/discover/movie?sort_by=popularity.desc&api_key=' + api_key;
 /*--search--*/
-const form = document.querySelector('.search-wrap')
-// const form = document.getElementById('form')
-const search = document.getElementById('search')
+
+const form = document.getElementById('form')
 const search_url= base_url + '/search/movie?' + api_key;
+const search = document.getElementById('search')
+const searching = document.querySelector('.searching')
+const mov_card_title = document.querySelector('.fav h1')
+// const arrow = document.querySelector('.movies-cont .arrow-btn')
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -15,48 +18,47 @@ form.addEventListener('submit',(e)=>{
     console.log(searchArea)
     if(searchArea){
         getSearch(search_url+'&query='+searchArea)
-        console.log(search_url+'&query='+searchArea)
+        // console.log(search_url+'&query='+searchArea)
     }
     else{
         getTrending(trend_url);
     }
 })
 
-getSearch(search_url)
-function getSearch(url){
-    fetch(url).then(res=>res.json()).then(data=>{
-        console.log(data.results);
-        showSearch(data.results);
-    })
-
+async function getSearch(url){
+    const resp = await fetch(url)
+    const responsed = await resp.json()
+    showSearch(responsed.results);
 }
 
-function showSearch(datas){
+async function showSearch(datas){
 
-    searching.innerHTML = ''
-    datas.forEach(data=>{
-        const {poster_path,title,vote_average,release_date,original_language} = data
-        const search_movie = document.createElement('div')
-        search_movie.classList.add("movies-card");
+    mov_card_title.innerHTML = `Search Results..`
 
-        search_movie.innerHTML = `
-        
-                    <img
-                    src="${img_url+poster_path}"
-                    alt="${title}"
-                    />
-                 <h3 class="movie-title">${title}</h3>
-                 <div><span class='${getcolor(vote_average)}'>Rating: ${vote_average}</span></div>
-                 
-                 <div class="hidden-movie-det">
-                    <div><span class='hl'>Movie Name:</span>${title}</div>
-                    <div><span class='hl'>Rating:</span> ${vote_average}</div>
-                    <div><span class="hl">Release Date:</span>${release_date}</div>
-                    <div><span class="hl">Languages:</span> ${original_language}</div>
-                    <button>See More</button>
-                 </div>
-                `
+    searching.innerHTML = datas.map(e=>{
+        return `
+        <div class="movies-card">
+        <img
+        src="${img_url+e.poster_path}"
+        alt="${e.title}"
+        />
 
-                searching.appendChild(search_movie)
-    })
+        <h3 class="movie-title">${e.title}</h3>
+     <div><span class='${getcolor(e.vote_average)}'>Rating: ${e.vote_average}</span></div>
+     
+     <div class="hidden-movie-det">
+        <div><span class='hl'>Movie Name:</span>${e.title}</div>
+        <div><span class='hl'>Rating:</span> ${e.vote_average}</div>
+        <div><span class="hl">Release Date:</span>${e.release_date}</div>
+        <div><span class="hl">Languages:</span> ${e.original_language}</div>
+        <button>See More</button>
+     </div>
+     </div>`
+    }).join(' ')
+
+    arrow.innerHTML=`<div class="arrow-btn">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+</div> `
+
 }
