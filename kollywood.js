8bbcda364af1c3705b7ff5b60294c7536065c3a9
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-03-03 23:47:59 +0530

    file->removed

--- kollywood.js
+++ kollywood.js
@@ -1,6 +1,7 @@
 /*---tamil movies---*/
 const tamil_url = 'https://api.themoviedb.org/3/discover/movie?api_key=0bd3a18560f450a76a32f637f6354d01&with_original_language=ta'
-const tamil = document.getElementById('tamil')
+const tamil = document.querySelector('.movies-cont-wrap-tam')
+const tam_arrow = document.querySelector('.mov-wrap .arrow-btn')
 
 getTamil(tamil_url)
 async function getTamil(url){
@@ -31,4 +32,11 @@ async function showTamil(datas){
      </div>
      </div>`
     }).join(' ')
+
+    tam_arrow.innerHTML=`<div class="arrow-btn">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+</div>`
+
+
 }
