1252a37d979d0dba2370d839c3cabf64854c43e8
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-03-20 23:41:15 +0530

    js->functions

--- js/script.js
+++ js/script.js
@@ -89,9 +89,9 @@ async function movie_card_fetch(id,i,text){
     let mov_card=''
     datas.forEach(e=>{
       e.poster_path = e.poster_path==null ? `./assets/cinema2.jpg` : `${e.poster_path}`;
-        console.log(e.id)
+        // console.log(e.id)
         mov_card +=`
-        <div class="movies-card">
+        <div class="movies-card" data-id=${e.id}>
         <img
         src="${img_url+e.poster_path}"
         alt="${e.title}"
@@ -100,27 +100,36 @@ async function movie_card_fetch(id,i,text){
         <h3 class="movie-title">${e.title}</h3>
      <div><span class='${getcolor(e.vote_average)}'>Rating: ${e.vote_average}</span></div>
      
-     <div class="hidden-movie-det">
+     <div class="hidden-movie-det" data-id=${e.id}>
         <div><span class='hl'>Movie Name:</span>${e.title}</div>
-        <div><span class='hl'>Rating:</span> ${e.vote_average}</div>
+        <div><span class='hl'>Rating:</span><span class="${getcolor(e.vote_average)}"> ${e.vote_average.toFixed(1)}</span></div>
         <div><span class="hl">Release Date:</span>${e.release_date}</div>
         <div><span class="hl">Languages:</span> ${e.original_language}</div>
-        <button>See More</button>
+        <button data-id=${e.id}>View</button>
      </div>
      </div>
      `;
     });
     card_det[i].innerHTML = mov_card;
     await mov_link( document.querySelectorAll('.hidden-movie-det button'));
+    await mov_link2(document.querySelectorAll('.hidden-movie-det'));
 }
 
-function mov_link(link_redirect){
-    link_redirect.forEach(d=>{
+function mov_link(redirect){
+    redirect.forEach(d=>{
       d.addEventListener('click',(e)=>{
-        window.location=`./movie.html?id=${835017}`;
+        window.location=`./movie.html?id=${e.currentTarget.dataset.id}`;
       })
     });
     }
+
+function mov_link2(redirect){
+    redirect.forEach(d=>{
+        d.addEventListener('click',(e)=>{
+            window.location = `./movie.html?id=${e.currentTarget.dataset.id}`
+        })
+    });
+}
 
 movie_card_fetch('en',0,'original_lanugage')
 movie_card_fetch(53,1,'genres')
@@ -129,64 +138,64 @@ movie_card_fetch(10752,3,'genres')
 movie_card_fetch(14,4,'genres')
 movie_card_fetch(420,5,'companies')
 
-// let isDown=false;
-// let slider_cont=Array.from(document.querySelectorAll('.mov-wrap'));
-// let startX;
-// let scrollLeft;
-// slider_cont.forEach(data=>{
-// data.addEventListener('mouseup',()=>{
-//     isDown=false;
-//     data.style.cursor='initial';
-// });
-// data.addEventListener('mousedown',(e)=>{
-//     startX=e.pageX-data.offsetLeft;
-//     isDown=true;
-//     scrollLeft=data.scrollLeft;
-//     data.style.cursor='initial';
-// })
-// data.addEventListener('mouseleave',()=>{
-//     isDown=false;
-//     data.style.cursor='initial';
-// })
-// data.addEventListener('mousemove',(e)=>{
-//     if(!isDown) return;
-//     e.preventDefault();
-//     const x=e.pageX-data.offsetLeft;
-//     const walk=(x-startX)*1;
-//     data.scrollLeft=scrollLeft-walk;
-//     data.style.cursor='grabbing';
-// })
-// data.addEventListener('touchstart',(e)=>{
-//     startX=e.touches[0].pageX-data.offsetLeft;
-//     isDown=true;
-//     scrollLeft=data.scrollLeft;
-// });
-// data.addEventListener('touchend',()=>{
-//     isDown=false;
-// })
-// data.addEventListener('touchleave',()=>{
-//     isDown=false;
-// })
-// data.addEventListener('touchmove',(e)=>{
-//     if(!isDown) return;
-//     const x=e.touches[0].pageX-data.offsetLeft;
-//     const walk=(Math.ceil(x)-startX);
-//     data.scrollLeft=scrollLeft-walk;
-// })
-// data.oncontextmenu = function (event) {
-//     event.preventDefault()
-//     event.stopPropagation()
-//     return false
-// }
-// });
-// const arrow_cont=document.querySelectorAll('.arrow-btn');
-// arrow_cont.forEach((arrow,index)=>{
-//     const data=arrow.querySelectorAll('svg');
-//     data.forEach((d,i)=>{
-//         d.addEventListener('click',
-//         (e)=>{
-//             const j=slider_cont[index];
-//             (i===1)?(j.scrollLeft=j.scrollLeft-(j.scrollLeft%332)+332):(j.scrollLeft%332?j.scrollLeft=j.scrollLeft-(j.scrollLeft%332):j.scrollLeft=j.scrollLeft-(j.scrollLeft%332)-332);
-//         })
-//     })
-// })
+let isDown=false;
+let slider_cont=Array.from(document.querySelectorAll('.mov-wrap'));
+let startX;
+let scrollLeft;
+slider_cont.forEach(data=>{
+data.addEventListener('mouseup',()=>{
+    isDown=false;
+    data.style.cursor='initial';
+});
+data.addEventListener('mousedown',(e)=>{
+    startX=e.pageX-data.offsetLeft;
+    isDown=true;
+    scrollLeft=data.scrollLeft;
+    data.style.cursor='initial';
+})
+data.addEventListener('mouseleave',()=>{
+    isDown=false;
+    data.style.cursor='initial';
+})
+data.addEventListener('mousemove',(e)=>{
+    if(!isDown) return;
+    e.preventDefault();
+    const x=e.pageX-data.offsetLeft;
+    const walk=(x-startX)*1;
+    data.scrollLeft=scrollLeft-walk;
+    data.style.cursor='grabbing';
+})
+data.addEventListener('touchstart',(e)=>{
+    startX=e.touches[0].pageX-data.offsetLeft;
+    isDown=true;
+    scrollLeft=data.scrollLeft;
+});
+data.addEventListener('touchend',()=>{
+    isDown=false;
+})
+data.addEventListener('touchleave',()=>{
+    isDown=false;
+})
+data.addEventListener('touchmove',(e)=>{
+    if(!isDown) return;
+    const x=e.touches[0].pageX-data.offsetLeft;
+    const walk=(Math.ceil(x)-startX);
+    data.scrollLeft=scrollLeft-walk;
+})
+data.oncontextmenu = function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+}
+});
+const arrow_cont=document.querySelectorAll('.arrow-btn');
+arrow_cont.forEach((arrow,index)=>{
+    const data=arrow.querySelectorAll('svg');
+    data.forEach((d,i)=>{
+        d.addEventListener('click',
+        (e)=>{
+            const j=slider_cont[index];
+            (i===1)?(j.scrollLeft=j.scrollLeft-(j.scrollLeft%332)+332):(j.scrollLeft%332?j.scrollLeft=j.scrollLeft-(j.scrollLeft%332):j.scrollLeft=j.scrollLeft-(j.scrollLeft%332)-332);
+        })
+    })
+})
