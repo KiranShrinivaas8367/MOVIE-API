aa385bd8bf6adf88a6db91ee314e977e41baeff5
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-03-25 13:07:05 +0530

    search-feature=>finished

--- js/search.js
+++ js/search.js
@@ -2,8 +2,8 @@
 // const base_url = 'https://api.themoviedb.org/3';
 // const img_url = 'https://image.tmdb.org/t/p/w500'
 
-// const trend_url = base_url + '/discover/movie?sort_by=popularity.desc&api_key=' + api_key;
 /*--search--*/
+
 const main = document.querySelector('.main')
 const form = document.getElementById('form')
 const search_url= base_url + '/search/movie?' + api_key;
@@ -12,8 +12,6 @@ const searching = document.querySelector('.searching')
 const mov_card_title = document.querySelector('.fav h1')
 const mov_home_btn = document.querySelector('.button')
 const mov_home_btn2 = document.querySelector('.button1')
-
-// const arrow = document.querySelector('.movies-cont .arrow-btn')
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -91,10 +89,4 @@ async function showSearch(datas){
               })
             });
             }
-
-//     arrow.innerHTML=`<div class="arrow-btn">
-//     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
-//     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
-// </div> `
-
 }
